5232804cb19fcbe7026e268dfc37ea3459c55300
Coder-crab <472245502@qq.com>
2021-12-17 16:14:24 +0800

    测试GITHUB

---
+++ test1/test1/test.c
@@ -0,0 +1,15 @@
+//写代码
+// 1.写出主函数（main函数）
+//printf -库函数-在屏幕上打印信息的
+//printf的使用，也得打招呼（引用头文件stdio.h）
+//编译+链接+运行代码
+//快捷键：ctrl+F5
+
+
+
+//#include <stdio.h>
+//int main()
+//{	
+//	printf("hello,world!\n");
+//	return 0;
+//}
---
+++ test1/test1/test2.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>;
+//int main()
+//{
+//	//字符类型
+//	char ch = 'a';
+//	//整型
+//	int age = 20;
+//	//短整型
+//	short num = 10;
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(double));
+//	
+//
+//	return 0;
+//}	
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a, &b);
+	sum = a + b;
+	printf("sum = %d\n",sum);
+	return 0;
+}
